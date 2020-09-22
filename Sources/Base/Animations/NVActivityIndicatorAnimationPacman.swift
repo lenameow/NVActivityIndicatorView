a7dacbbf0e8f6eb4aaf160eaf06def3c5cd97c4b
Lena Hu <aileen9103@gmail.com>
2020-09-22 22:50:47 +1000

    test 233

--- Sources/Base/Animations/NVActivityIndicatorAnimationPacman.swift
+++ Sources/Base/Animations/NVActivityIndicatorAnimationPacman.swift
@@ -106,7 +106,7 @@ class NVActivityIndicatorAnimationPacman: NVActivityIndicatorAnimationDelegate {
         animation.isRemovedOnCompletion = false
 
         // Draw circles
-        let circle = NVActivityIndicatorShape.circle.layerWith(size: CGSize(width: circleSize, height: circleSize), color: color)
+        let circle = NVActivityIndicatorShape.circle.layerWith(x: 0, y: 0, size: CGSize(width: circleSize, height: circleSize), color: color)
         let frame = CGRect(
             x: (layer.bounds.size.width - size.width) / 2 + size.width - circleSize,
             y: (layer.bounds.size.height - circleSize) / 2,
